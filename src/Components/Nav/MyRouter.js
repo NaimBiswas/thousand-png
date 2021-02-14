dc5c49692739f5121c4dc4d9ffa4374787870c3e
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-14 20:47:06 +0600

    set login and regiter route

--- src/Components/Nav/MyRouter.js
+++ src/Components/Nav/MyRouter.js
@@ -10,6 +10,8 @@ const Home = lazy(() => import('../Pages/Home'))
 const Portfolio = lazy(() => import('../Pages/Portoflio'))
 const Contact = lazy(() => import('../Pages/Contact'))
 const About = lazy(() => import('../Pages/About'))
+const Login = lazy(() => import('../Pages/Login'))
+const Regsiter = lazy(() => import('../Pages/Register'))
 
 const MyRouter = () => {
    return (
@@ -24,6 +26,7 @@ const MyRouter = () => {
                <Home></Home>
             </Suspense>
          </Route>
+
          <Route path='/about' component={About} >
             <Suspense fallback={<div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems: 'center', justifyContent: 'center' }}>
 
@@ -33,6 +36,7 @@ const MyRouter = () => {
                <About></About>
             </Suspense>
          </Route>
+
          <Route path='/portoflio' component={Portfolio} >
             <Suspense fallback={<div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems: 'center', justifyContent: 'center' }}>
 
@@ -42,6 +46,7 @@ const MyRouter = () => {
                <Portfolio></Portfolio>
             </Suspense>
          </Route>
+
          <Route path='/contact' component={Contact} >
             <Suspense fallback={<div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems: 'center', justifyContent: 'center' }}>
 
@@ -49,6 +54,30 @@ const MyRouter = () => {
                <h3 className='text-info h2 italic'>Please Wait...</h3>
             </div>}>
                <Contact></Contact>
+            </Suspense>
+         </Route>
+
+
+         {/* Login  */}
+         <Route path='/contact' component={Contact} >
+            <Suspense fallback={<div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+
+               <RingLoader color={"#D0021B"} size={100} />
+               <h3 className='text-info h2 italic'>Please Wait...</h3>
+            </div>}>
+               <Login></Login>
+            </Suspense>
+         </Route>
+
+
+         {/* Register  */}
+         <Route path='/contact' component={Contact} >
+            <Suspense fallback={<div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+
+               <RingLoader color={"#D0021B"} size={100} />
+               <h3 className='text-info h2 italic'>Please Wait...</h3>
+            </div>}>
+               <Regsiter></Regsiter>
             </Suspense>
          </Route>
 
