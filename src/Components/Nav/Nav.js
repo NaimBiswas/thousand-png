b8988226792260c3b39d4211bcc8b523934a38ea
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-16 13:24:44 +0600

    set Title

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -12,7 +12,7 @@ const OurNav = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                <Nav className="mr-auto">
-                  <NavLink className="nav-link" to="/">
+                  <NavLink exact className="nav-link" to="/">
                      Home
                        </NavLink>
                   <NavLink className="nav-link" to="/unsplash">
