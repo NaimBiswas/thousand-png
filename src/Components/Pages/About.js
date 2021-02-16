20160b99ab386dba03aa867fce9ee503f13be29b
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-16 13:37:38 +0600

    set next a perpage on Unsplash

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,6 +1,7 @@
 
 import React, { useEffect, useState } from 'react'
-import { Spinner } from 'react-bootstrap'
+import { Button, Spinner } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 // 563492ad6f9170000100000175e4e75afcad4164830aefd55d797d28
 // xKGqKlXxfQZKg4ZnM-XKt3iW9xK2TB8OO1LeOVJjnFE
 // ExySIyO6CZyXtnMrbiLLl0s2R8Uo8YvE2Q8u14ZRi9U
@@ -22,16 +23,18 @@ const About = () => {
 
          )
    }
+   const PrePage = () => {
+      setCount(Count - 1)
+      setImage('')
+      fetch(`https://api.unsplash.com/photos/?page=${Count}&per_page=40&client_id=${ACCESS_KEy}`)
+         .then((res) => res.json())
+         .then(
+            (results) => setImage(results)
+         )
+
+   }
    useEffect(() => {
       fetch(`https://api.unsplash.com/photos/?per_page=1&per_page=40&client_id=${ACCESS_KEy}`).then((res) => res.json()).then((results) => setImage(results))
-
-
-      // fetch(`https://api.unsplash.com/search/photos/?page=12&query=${query}&client_id=${ACCESS_KEy}`)
-      //    .then((res) => res.json())
-      //    .then(
-      //       (results) => setCate(results.results)
-      //    )
-
 
    }, [])
 
@@ -64,8 +67,14 @@ const About = () => {
          } */}
          <div className="container">
             <div className="row justify-content-between" style={{ marginTop: '50px', marginBottom: '70px' }}>
-               <button className='btn btn-danger'>Previous</button>
-               <button className='btn btn-info' onClick={NextPages}>Next</button>
+
+               {
+                  Count === 2 ? '' : <Link to='/unsplash'> <Button onClick={PrePage} variant='warning'>Previous</Button></Link>
+               }
+               <Link to='/unsplash'> <button className='btn btn-info' onClick={NextPages}>Next</button></Link>
+
+
+
             </div>
          </div>
       </div >
